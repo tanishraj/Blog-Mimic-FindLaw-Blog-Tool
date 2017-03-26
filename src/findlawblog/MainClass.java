cea15a9e7ea68c8b64f6d43ccfdedf1c900b26ff
tanish_raj <tanish.quitegrowth@gmail.com>
2017-03-26 08:30:17 +0530

    Final Project is uploaded to repo.

---
+++ src/findlawblog/MainClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findlawblog;
+
+/**
+ *
+ * @author UC202958
+ */
+public class MainClass {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            
+            new MainFrame().setVisible(true);
+        });
+    }
+}
